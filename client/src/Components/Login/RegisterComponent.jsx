068b73d92f8578454fc841db61a38eff18fbc628
geekySapien <escaping.blurred.reality@gmail.com>
2021-12-29 01:47:28 +0530

    integrated client side with server for signup

--- client/src/Components/Login/RegisterComponent.jsx
+++ client/src/Components/Login/RegisterComponent.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 import "./Login.css";
 const RegisterComponent = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/signup", {
+        name,
+        email,
+        password
+      });
+      console.log(res);
+      res.data && window.location.replace("/signin");
+    } catch (err) {
+
+      console.log(err);
+    }
+  }
   return (
     <>
       <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
         <div className="border-2 border-gray-200 p-8 flex flex-col items-center gap-6 w-max">
           <span className="fontStyle  text-3xl">Socialice</span>
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <input
               type="text"
               className="border-2 bg-gray-50 p-2 md:w-72"
               placeholder="Username"
+              value={name}
+              onChange={(e)=>setName(e.target.value)}
             />
             <input
               type="email"
               className="border-2 bg-gray-50 p-2 md:w-72"
               placeholder="Email"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
             />
             <input
               type="password"
               className="border-2 bg-gray-50 p-2 md:w-72"
               placeholder="Password"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
             />
             <button
               type="submit"
