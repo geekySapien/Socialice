f636776f170249ceceab7fd461e456682bf857dd
geekySapien <escaping.blurred.reality@gmail.com>
2021-12-30 23:25:23 +0530

    Added the navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,14 @@ import RegisterComponent from './Components/Login/RegisterComponent'
 import ProfileComponent from './Components/Profile/ProfileComponent';
 import { PostsComponent } from './Components/Posts/PostsComponent';
 import CreatePostComponent from './Components/CreatePost/CreatePostComponent';
+import Navbar from './Components/Navbar/Navbar.component';
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
+          
           <Route path="/signin" element={<LoginComponent />} />
           <Route path="/signup" element={<RegisterComponent />} />
           <Route path="/profile" element={<ProfileComponent />} />
---
+++ client/src/Components/Navbar/Navbar.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-screen flex sticky top-0 z-50 bg-white h-12 shadow-md shadow-gray-500/50">
+        <div className="flex w-1/2 fontStyle text-lg md:text-3xl pl-6 items-center  cursor-pointer">
+         <Link to="/">Socialice</Link>
+        </div>
+        <div
+          className="w-1/2 flex justify-evenly py-2 gap-2 md:gap-3 text-xs md:text-lg"
+          style={{ fontFamily: "Lora" }}
+        >
+          <Link to="/signin">
+            <p className="cursor-pointer">Signin</p>
+          </Link>
+          <Link to="/signup">
+            <p className="cursor-pointer">Signup</p>
+          </Link>
+          <Link to="/profile">
+            <p className="cursor-pointer">Profile</p>
+          </Link>
+          <Link to="/create">
+            <p className="cursor-pointer">Create Post</p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
